4267e93491dcd8c978663242c536a885d960998e
HenriqueAlmeidaLucareli <henriquelucareli@gmail.com>
2026-03-30 20:53:13 -0300

    Aula 03 sobre python na FIAP

---
+++ aula02_variaveis_operadores/aula02b-operadores.py
@@ -0,0 +1,38 @@
+num1 = 5
+num2 = 2
+
+print(type(num1),type(num2))
+
+res_operacao= num1/num2
+print(res_operacao,type(res_operacao))
+
+num= 15
+print() # pular linha
+
+num=num+2
+print(num)
+
+num+=2
+print(num)
+
+num-=2
+print(num)
+
+print(7==6)
+
+print(6>=6)
+
+idade=20
+print(idade>=21)
+
+logado= True
+print(logado,type(logado))
+
+mairo_idade= idade>=18
+print(mairo_idade,type(mairo_idade))
+
+# STRING
+nom1= 'Marcos'
+nom2= 'marcos'
+print(nom1.upper() == nom2.upper())
+
